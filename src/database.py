1a5b65e147a40aeb462aa6391db289fd040be5e9
Lucas Meneses <lucassmeneses0708@gmail.com>
2026-08-18 19:16:11 -0300

    ci: add lint step; fix flake8 findings; make engine creation lazy

--- src/database.py
+++ src/database.py
@@ -1,20 +1,15 @@
 from sqlalchemy import create_engine
-from config import (
-    DB_HOST,
-    DB_PORT,
-    DB_NAME,
-    DB_USER,
-    DB_PASSWORD
-)
+from config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 
 engine = None
+
 
 def get_engine():
     global engine
     DATABASE_URL = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+        f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
+        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
     if engine is None:
-        engine = create_engine(DATABASE_URL) 
+        engine = create_engine(DATABASE_URL)
     return engine
